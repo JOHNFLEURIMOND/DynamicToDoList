2e7f11cd207e0906d1e7b78601d23de5116dc002
johnfleurimond <johncfleurimond@gmail.com>
2022-02-22 19:53:25 -0500

    scrap footer

--- components/App.jsx
+++ components/App.jsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import Nav from '../components/Navbar/Nav.jsx';
-import MainHero from '../components/MainHero/MainHero';
-import About from '../components/About/About';
-import ToDoList from './ToDoList/ToDoList';
-import CompletedListPage from './ToDoList/CompletedListPage';
-import {
-  Link, Route, BrowserRouter as Router, Switch
-} from 'react-router-dom';
-import { GlobalStyle, Container } from './layout/global-style';
+import React from "react";
+import Nav from "../components/Navbar/Nav.jsx";
+import MainHero from "../components/MainHero/MainHero";
+import About from "../components/About/About";
+import ToDoList from "./ToDoList/ToDoList";
+import CompletedListPage from "./ToDoList/CompletedListPage";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { GlobalStyle, Container } from "./layout/global-style";
 
-const Homepage = (props) => {
-
+const Homepage = props => {
   return (
     <Container>
       <GlobalStyle />
-
       <Nav />
-     
       <MainHero />
-     
       <ToDoList />
     </Container>
   );
